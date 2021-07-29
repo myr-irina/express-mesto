39a585b33241a69ea5132a5f2b30b817356538a5
Irina Aleksandrova <myr-irina2@yandex.ru>
2021-07-29 21:27:23 +0300

    создание роутов для карточек

--- routes/cards.js
+++ routes/cards.js
@@ -1,2 +1,11 @@
-// const router = require('express').Router();
+/* eslint-disable no-undef */
+const router = require("express").Router();
+const {getCards, createCard, deleteCard} = require('../controllers/cards');
+
+
+router.get('/cards', getCards);
+router.post('/cards', createCard);
+router.delete('/cards/:cardId', deleteCard);
+
+module.exports = router;
 
