ffa5693c8e71463a22b9fdf176479c676fd0b074
Irina Aleksandrova <myr-irina2@yandex.ru>
2021-07-29 21:38:54 +0300

    добавила cardsRouter в app.js

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 // const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards')
 // eslint-disable-next-line no-unused-vars
-const { PORT = 3000, BASE_PATH } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 // подключаемся к серверу mongo
@@ -19,8 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-app.use('/', usersRouter);
-
 app.use((req, res, next) => {
   req.user = {
     _id: '6102ca6d5d45382ab85bbaf2'
@@ -28,6 +27,9 @@ app.use((req, res, next) => {
 
   next();
 });
+
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
